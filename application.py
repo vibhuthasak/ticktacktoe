33c1e60fc218e8d33c1f4093d7e2fb4029189268
Brian Yu <brian@cs50.harvard.edu>
2019-01-20 14:31:50 -0500

    Prepare to deploy to Heroku

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+
+    if "board" not in session:
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["turn"] = "X"
+
+    return render_template("game.html", game=session["board"], turn=session["turn"])
+
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+    return redirect(url_for("index"))
