f210220dada027aa3695570155abd4f5012e0c2c
Vibhutha Kumarage <vkartz@gmail.com>
2019-01-26 22:04:23 +0530

    Game winning and winner function added

--- application.py
+++ application.py
@@ -15,8 +15,8 @@ def index():
     if "board" not in session:
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
-
-    return render_template("game.html", game=session["board"], turn=session["turn"])
+        session["winner"] = None
+    return render_template("game.html", game=session["board"], turn=session["turn"], winner= session["winner"])
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
@@ -32,3 +32,22 @@ def play(row, col):
     return redirect(url_for("index"))
 
     return redirect(url_for("index"))
+def isOver(board):
+  # Row win
+  for row in board:
+    if row[0] is not None and (row[0] == row[1] == row[2]):
+      return True
+    
+  # Col win
+  for col in range(3):
+    if board[0][col] is not None and (board[0][col] == board[1][col] == board[2][col]):
+      return True
+
+  # Cross win
+  if(board[0][0] is not None and (board[0][0] == board[1][1] == board[2][2])):
+    return True
+  
+  if(board[0][2] is not None and (board[0][2] == board[1][1] == board[2][0])):
+    return True
+
+  return False
